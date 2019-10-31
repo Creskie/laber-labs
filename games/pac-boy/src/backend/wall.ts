f3ae788d949775f162382d5058ceca6a90e5eff9
Lisa Wong <ljwong@ncsu.edu>
2019-10-30 21:46:32 -0400

    gitignore all README.md (1)

--- games/pac-boy/src/backend/wall.ts
+++
@@ -1,9 +0,0 @@
-export class Wall {
-    readonly tiles: Array<number>;
-    readonly closed: boolean;
-
-    constructor(tiles: Array<number>, closed: boolean) {
-        this.tiles = tiles;
-        this.closed = closed;
-    }
-}
